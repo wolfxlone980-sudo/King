24094ca7a094b09db8c680b877477f050367dd50
wolfxlone980-sudo <wolfxlone980@gmail.com>
2026-08-03 23:03:39 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function nextPage() {
+    document.getElementById("welcome").style.display = "none";
+    document.getElementById("passwordPage").style.display = "flex";
+}
 
+function checkPassword() {
+    let pass = document.getElementById("password").value.toLowerCase().trim();
+
+    if (pass === "favourite chapter") {
+        alert("Welcome to your Favourite Chapter ❤️");
+    } else {
+        alert("Wrong Password 💔");
+    }
+}
