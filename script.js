e3f1e8aac9aac811686f8e1c029bd91160faea9d
wolfxlone980-sudo <wolfxlone980@gmail.com>
2026-08-04 10:31:32 +0600

    Update script.js

--- script.js
+++ script.js
@@ -3,62 +3,102 @@ function nextPage() {
     document.getElementById("passwordPage").style.display = "flex";
 }
 
+
 function checkPassword() {
-    let pass = document.getElementById("password").value.toLowerCase().trim();
+
+    let pass = document
+        .getElementById("password")
+        .value
+        .toLowerCase()
+        .trim();
 
     if (pass === "favourite chapter") {
+
         document.getElementById("passwordPage").style.display = "none";
         document.getElementById("envelopePage").style.display = "flex";
+
     } else {
+
         alert("Wrong Password 💔");
+
     }
 }
 
+
 function openEnvelope() {
+
     document.getElementById("envelopePage").style.display = "none";
     document.getElementById("letterPage").style.display = "flex";
 
     document.getElementById("nextChapterBtn").style.display = "none";
 
     startLetter();
+
 }
 
+
 const lines = [
+
     "Hey tui... ❤️",
+
     "Haa... tokei bolchi. 😊",
+
     "Aj tor Birthday... 🎂",
+
     "Hoyto vabchis...",
+
     "Eta sudhu ekta website na...",
+
     "Eta amar tor jonno banano ekta chotto surprise. 🤍",
+
     "Tai aste aste por...",
+
     "Golpota ekhono shesh hoyni... ✨"
+
 ];
+
 
 let line = 0;
 
+
 function startLetter() {
+
     line = 0;
+
     document.getElementById("typewriter").innerHTML = "";
+
     showNextLine();
+
 }
+
 
 function showNextLine() {
 
     if (line >= lines.length) {
-        document.getElementById("nextChapterBtn").style.display = "block";
+
+        setTimeout(function() {
+
+            document.getElementById("nextChapterBtn").style.display = "block";
+
+        }, 500);
+
         return;
     }
 
+
     let text = lines[line];
+
     let i = 0;
 
     let box = document.getElementById("typewriter");
 
-    let typing = setInterval(() => {
+
+    let typing = setInterval(function() {
 
         box.innerHTML += text.charAt(i);
 
         i++;
+
 
         if (i >= text.length) {
 
@@ -76,6 +116,9 @@ function showNextLine() {
 
 }
 
+
 function goToChapter2() {
-    alert("🚧 Chapter 2 is coming soon ❤️");
+
+    alert("Chapter 2 is coming soon ❤️");
+
 }
